5c453315188c987d86ee1e377bce91cf0273e1d9
0IIIIM0 <ignac23med@gmail.com>
2022-03-23 11:59:44 -0600

    Initial commit

---
+++ src/Mounstruo.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+
+public class Mounstruo {
+    public class Mounstruo extends Personaje{
+        //tieneCUernos 
+        //tieneTentaculos
+        // colores
+        // habilidades
+
+        //espantar
+        //trabajar
+        //hacerReir
+        
+        private boolean tieneCuernos;
+        private boolean tieneTentaculos;
+        private String color;
+        private ArrayList<String> habilidades;
+        
+        public Mounstruo(String nombre, double estatura, double peso){
+            super(nombre,estatura,peso);
+        }
+
+        public boolean isTieneCuernos() {
+            return tieneCuernos;
+        }
+
+        public void setTieneCuernos(boolean tieneCuernos) {
+            this.tieneCuernos = tieneCuernos;
+        }
+
+        public boolean isTieneTentaculos() {
+            return tieneTentaculos;
+        }
+
+        public void setTieneTentaculos(boolean tieneTentaculos) {
+            this.tieneTentaculos = tieneTentaculos;
+        }
+
+        public String getColor() {
+            return color;
+        }
+
+        public void setColor(String color) {
+            this.color = color;
+        }
+
+        public ArrayList<String> getHabilidades() {
+            return habilidades;
+        }
+
+        public void setHabilidades(ArrayList<String> habilidades) {
+            this.habilidades = habilidades;
+    
+        }
+
+        public void espantar (){
+            System.out.println("boooo!!");
+        }
+        public void trabajar(){
+            System.out.println("estoy trabajando");
+        }
+        public void hacerReir(){
+            System.out.println("JAJAJA");
+
+        }
+
+        public Mounstruo(String nombre, double estatura, double peso, boolean tieneCuernos, boolean tieneTentaculos, String color, ArrayList<String> habilidades) {
+            super(nombre, estatura, peso);
+            this.tieneCuernos = tieneCuernos;
+            this.tieneTentaculos = tieneTentaculos;
+            this.color = color;
+            this.habilidades = habilidades;
+        }
+    }
+    
+    
+}
---
+++ src/Personaje.java
@@ -0,0 +1,57 @@
+public class Personaje { //clase padre quien hereda
+    //Atributos
+    // nombre
+    // estatura
+    //peso
+    //Metodos
+    //comer dormir pensar
+
+    protected String nombre;
+    protected double estatura;
+    protected double peso;
+
+
+    public void comer(){
+        System.out.println("comiendo");
+    }
+
+    public void dormir(){
+        System.out.println("Durmiendo");
+    }
+    public void pensar(){
+        System.out.println("Pensando");
+    }
+
+
+    public Personaje(String nombre, double estatura, double peso) {
+        this.nombre = nombre;
+        this.estatura = estatura;
+        this.peso = peso;
+        System.out.println("eeste es el constructor padre");
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(double estatura) {
+        this.estatura = estatura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+}
